189fdd81d4eb62dcea2a13b1d37b0b9418afa4ed
walterbega-sbx <walter.pedro@superbid.net>
2025-07-08 07:24:15 -0300

    feat: Adiciona aplicativo de calculadora básica

---
+++ src/calculadora.js
@@ -0,0 +1,12 @@
+
+const somar = (a, b) => a + b;
+const subtrair = (a, b) => a - b;
+const multiplicar = (a, b) => a * b;
+const dividir = (a, b) => a / b;
+
+module.exports = {
+  somar,
+  subtrair,
+  multiplicar,
+  dividir,
+};
