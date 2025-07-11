ac871a21624ae861f67ede2cde7e47834b22044d
walterbega-sbx <walter.pedro@superbid.net>
2025-07-11 16:22:26 -0300

    feat: Adiciona endpoint de percentual

--- src/controllers/calculadoraController.js
+++ src/controllers/calculadoraController.js
@@ -1,35 +1,41 @@
-const calculadora = require('../calculadora');
-
 const somar = (req, res) => {
-    const { num1, num2 } = req.body;
-    const resultado = calculadora.somar(num1, num2);
+    const { valor1, valor2 } = req.body;
+    const resultado = valor1 + valor2;
     res.send({ resultado });
-};
+}
 
 const subtrair = (req, res) => {
-    const { num1, num2 } = req.body;
-    const resultado = calculadora.subtrair(num1, num2);
+    const { valor1, valor2 } = req.body;
+    const resultado = valor1 - valor2;
     res.send({ resultado });
-};
+}
 
 const multiplicar = (req, res) => {
-    const { num1, num2 } = req.body;
-    const resultado = calculadora.multiplicar(num1, num2);
+    const { valor1, valor2 } = req.body;
+    const resultado = valor1 * valor2;
     res.send({ resultado });
-};
+}
 
 const dividir = (req, res) => {
-    const { num1, num2 } = req.body;
-    if (num2 === 0) {
-        return res.status(400).send({ error: 'Não é possível dividir por zero' });
+    const { valor1, valor2 } = req.body;
+    if (valor2 === 0) {
+        res.status(400).send({ erro: "não é possível dividir por zero" });
+    } else {
+        const resultado = valor1 / valor2;
+        res.send({ resultado });
     }
-    const resultado = calculadora.dividir(num1, num2);
+}
+
+const percentual = (req, res) => {
+    const { valor, percentual } = req.body;
+    const resultado = (valor * percentual) / 100;
     res.send({ resultado });
-};
+}
 
 module.exports = {
     somar,
     subtrair,
     multiplicar,
-    dividir
-};
+    dividir,
+    percentual
+}
